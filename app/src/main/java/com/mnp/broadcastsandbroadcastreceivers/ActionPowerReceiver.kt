81737f6672ab6097041323beccee5cc9c84c5880
nm86nm <nm86nm@gmail.com>
2025-02-04 17:07:31 +0100

    Project Improvement

--- app/src/main/java/com/mnp/broadcastsandbroadcastreceivers/ActionPowerReceiver.kt
+++ app/src/main/java/com/mnp/broadcastsandbroadcastreceivers/ActionPowerReceiver.kt
@@ -5,7 +5,7 @@ import android.content.Context
 import android.content.Intent
 import android.widget.Toast
 
-class ActionPowerReceiver : BroadcastReceiver() {
+class ActionPowerReceiver{
     val actionPowerConnected: BroadcastReceiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context, intent: Intent) {
             Toast.makeText(context, "Device connected to charger", Toast.LENGTH_LONG).show()
@@ -16,15 +16,5 @@ class ActionPowerReceiver : BroadcastReceiver() {
         override fun onReceive(context: Context, intent: Intent) {
             Toast.makeText(context, "Device disconnected from charger", Toast.LENGTH_LONG).show()
         }
-    }
-
-    override fun onReceive(context: Context?, intent: Intent?) {
-        /*if (intent?.action == "android.intent.action.ACTION_POWER_CONNECTED") {
-            Toast.makeText(context, "Device connected to charger", Toast.LENGTH_LONG).show()
-            context?.registerReceiver(actionPowerConnected, IntentFilter("android.intent.action.ACTION_POWER_CONNECTED"))
-        } else if(intent?.action == "android.intent.action.ACTION_POWER_DISCONNECTED"){
-            Toast.makeText(context, "Device disconnected from charger", Toast.LENGTH_LONG).show()
-            context?.registerReceiver(actionPowerDisconnected, IntentFilter("android.intent.action.ACTION_POWER_DISCONNECTED"))
-        }*/
     }
 }
--- app/src/main/java/com/mnp/broadcastsandbroadcastreceivers/MainActivity.kt
+++ app/src/main/java/com/mnp/broadcastsandbroadcastreceivers/MainActivity.kt
@@ -1,20 +1,14 @@
 package com.mnp.broadcastsandbroadcastreceivers
 
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.content.Intent
 import android.content.IntentFilter
 import android.os.Build
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.mnp.broadcastsandbroadcastreceivers.ui.theme.BroadcastsAndBroadcastReceiversTheme
 
@@ -32,22 +26,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ActionPowerReceiver()
-
                     registerReceiver(ActionPowerReceiver().actionPowerConnected, IntentFilter("android.intent.action.ACTION_POWER_CONNECTED"))
                     registerReceiver(ActionPowerReceiver().actionPowerDisconnected, IntentFilter("android.intent.action.ACTION_POWER_DISCONNECTED"))
                 }
             }
         }
     }
-}
-
-@Composable
-fun connected(){
-    Text("Connected")
-}
-
-@Composable
-fun disconnected(){
-    Text("Disconnected")
 }
